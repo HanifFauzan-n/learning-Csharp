608a45483c369cd102663ce42aab05c531a25156
HanifFauzan-n <haniffauzann55@gmail.com>
2025-08-04 14:51:00 +0700

    Update version bootstrap and change fiture

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,38 +1,41 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-document.addEventListener('DOMContentLoaded',()=>{
+﻿document.addEventListener('DOMContentLoaded', function () {
+    
+    console.log("DEBUG: DOM Siap. Script todolist dimulai.");
 
     const themeToggleBtn = document.getElementById('theme-toggle');
-    const body = document.body;
+    const htmlElement = document.documentElement;
+    
+    console.log("DEBUG: Mencari tombol...", themeToggleBtn, " isi html",htmlElement);
 
-    if(!themeToggleBtn){
+    if (!themeToggleBtn) {
+        console.error("DEBUG: Tombol #theme-toggle TIDAK DITEMUKAN. Script berhenti.");
         return;
     }
+
     const themeIcon = themeToggleBtn.querySelector('i');
 
     function applyTheme(theme) {
-        if(theme === 'dark') {
-            body.classList.add('dark-mode');
+        htmlElement.setAttribute('data-bs-theme', theme);
+        if (theme === 'dark') {
             themeIcon.classList.remove('fa-moon');
             themeIcon.classList.add('fa-sun');
-        }
-        else {
-            body.classList.add('light-mode');
+        } else {
             themeIcon.classList.remove('fa-sun');
             themeIcon.classList.add('fa-moon');
         }
-
-        const saveTheme = localStorage.getItem('theme' || 'light');
-        applyTheme(saveTheme);
-
-        themeToggleBtn.addEventListener('click', ()=> {
-            const currentTheme = body.classList.contains('dark-mode') ? 'dark' : 'light';
-            const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-
-            applyTheme(newTheme);
-            localStorage.setItem('theme', newTheme);
-        });
     }
-})
+
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    applyTheme(savedTheme);
+    console.log("DEBUG: Tema awal diterapkan:", savedTheme);
+
+    themeToggleBtn.addEventListener('click', function () {
+        console.log("DEBUG: Tombol BERHASIL diklik!");
+        const currentTheme = htmlElement.getAttribute('data-bs-theme') || 'light';
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+        applyTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
+        console.log("DEBUG: Tema diubah dan disimpan:", newTheme);
+    });
+});
